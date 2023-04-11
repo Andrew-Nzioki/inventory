454bada94aaea57586d5402a3bccd3611fc7da27
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-11 15:16:09 +0300

    validation for fields added

---
+++ Readme.md
@@ -0,0 +1,36 @@
+Project Name: Build a dynamic and interactive online store using JavaScript.
+
+Project Overview: The application will have the following features:
+
+Users can view a list of products available for purchase.
+Users can filter products by category, price range, and other parameters.
+Users can add products to a shopping cart and modify the quantity.
+Users can checkout and enter their shipping and payment information.
+Users can receive confirmation of their order and a receipt.
+Technical Skills Demonstrated:
+
+Advanced JavaScript concepts such as closures, prototypes, and ES6 features.
+DOM manipulation using JavaScript.
+Asynchronous JavaScript and handling of promises.
+Use of APIs to fetch product data and handle payments.
+Use of modern front-end frameworks like React, Vue, or Angular.
+Technology Stack:
+
+HTML, CSS for front-end.
+JavaScript for functionality.
+React, Vue, or Angular for building the front-end.
+API integration for fetching product data and handling payments.
+Project Deliverables:
+
+A fully functional online store that meets the above requirements.
+A codebase that is well-organized, commented, and follows best practices.
+Documentation on how to use the application and any special features.
+Integration with a payment gateway such as PayPal or Stripe.
+Project Timeline:
+
+Week 1: Project planning and research.
+Week 2: Develop front-end UI using HTML, CSS, and chosen framework.
+Week 3: Implement JavaScript functionality for product filtering, shopping cart, and checkout process.
+Week 4: Implement API integration for fetching product data and handling payments.
+Week 5: Testing and debugging.
+Week 6: Finalizing documentation and project submission.
--- index.js
+++ index.js
@@ -6,22 +6,70 @@ function getAllProducts(){
  .then(res=>res.json())//turn the data into json
  .then(products=>
     {
-    console.log(products)//check whether the data is loaded
+    //console.log(products)//check whether the data is loaded
     products.map(product=>{
-        
-       console.log(product.name)
-    })   
-    })
+            
+
+       let productCard=document.createElement('table')
+       productCard.innerHTML=`
+       <thead>
+       <tr>
+         <th scope="col">ID</th>
+         <th scope="col">Product Name</th>
+         <th scope="col">Quantity</th>
+         <th scope="col">Price</th>
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <th scope="row">${product.id}</th>
+         <td> ${product.name}</td>
+         <td> ${product.quantity}</td>
+         <td>$${product.price}</td>
+       </tr>
+     </tbody>`
+
+    productCard.className='table'
+    document.querySelector('.container').appendChild(productCard) //add products to the DOM
+     let productOption=document.createElement('option')
+     productOption.innerHTML=`
+     <option value="${product.id && product.name} " id="productOption">
+     `
+     document.getElementById('datalistOptions').appendChild(productOption)
+})})}
+
+document.querySelector("#add-productForm").addEventListener("submit", handleAddProduct);
+//function to get the data from the form
+function handleAddProduct(e){
+e.preventDefault()
+
+//adding validation to prevent empty insertion of data
+if (!e.target.name.value || !e.target.price.value) {
+    alert("Please fill in both fields!");
+    return;
+  }
+let productObj = {
+    name: e.target.name.value,
+    quantity: e.target.qty.value,
+    price: e.target.price.value,
+    img: e.target.img.value,
+    description: e.target.description.value
+  };
+ console.log(productObj)
+ addProducts(productObj);
 }
 
-function addProducts(){
-    fetch(`${url}`,{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(productObj)
+//Function to perform a POST request to the server
+function addProducts(productObj) {
+    console.log(JSON.stringify(productObj));
+    //fetch takes three parameters, url,{methods and headers}, then the body of the POST incase of a Post request
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productObj),
     })
-}
-
+      .then((res) => res.json())
+  }
 window.addEventListener('load',getAllProducts)
